74cd657181d1160982305771addf063aff198774
ct-dixit <106728438+ct-dixit@users.noreply.github.com>
2023-03-25 10:06:00 +0530

    InfiniteScroll add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const pageSize = 6;
-  const country = "in";
+  const country = 'in';
   return (
     <>
       <BrowserRouter>
@@ -13,12 +13,20 @@ function App() {
         <Routes>
           <Route exact path="/" element={<News key="general" pageSizes={pageSize} country={country} category="general" />} />
           <Route exact path="/business" element={<News key="business" pageSizes={pageSize} country={country} category="business" />} />
-          <Route exact path="/entertainment" element={<News key="entertainment" pageSizes={pageSize} country={country} category="entertainment" />} />
+          <Route
+            exact
+            path="/entertainment"
+            element={<News key="entertainment" pageSizes={pageSize} country={country} category="entertainment" />}
+          />
           <Route exact path="/general" element={<News key="general" pageSizes={pageSize} country={country} category="general" />} />
           <Route exact path="/health" element={<News key="health" pageSizes={pageSize} country={country} category="health" />} />
           <Route exact path="/science" element={<News key="science" pageSizes={pageSize} country={country} category="science" />} />
           <Route exact path="/sports" element={<News key="sports" pageSizes={pageSize} country={country} category="sports" />} />
-          <Route exact path="/technology" element={<News key="technology" pageSizes={pageSize} country={country} category="technology" />} />
+          <Route
+            exact
+            path="/technology"
+            element={<News key="technology" pageSizes={pageSize} country={country} category="technology" />}
+          />
         </Routes>
       </BrowserRouter>
     </>
--- src/component/News.js
+++ src/component/News.js
@@ -1,65 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import NewsItem from './NewsItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props) => {
-  const [loading, setLoading] = useState(false);
   const [article, setArticle] = useState([]);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
 
   const capitalizeFirstLatter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
   useEffect(() => {
     const fetchNews = async () => {
       let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=160b435514254bb28c355f4814de1b80&page=1&pageSize=${props.pageSizes}`;
-      setLoading(true);
+
       let data = await fetch(url);
       let parsedData = await data.json();
       setArticle(parsedData.articles);
       setTotalResults(parsedData.totalResults);
-      setLoading(false);
-      document.title = `${capitalizeFirstLatter(props.category)} - News Live App`
+
+      document.title = `${capitalizeFirstLatter(props.category)} - News Live App`;
     };
 
     fetchNews(); // Call fetchNews function inside the useEffect
   }, [props.country, props.category, props.pageSizes]);
 
-  const commonUse = async () => {
+  // const commonUse = async () => {
+  //   let data = await fetch(
+  //     `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=160b435514254bb28c355f4814de1b80&page=${page}&pageSize=${props.pageSizes}`
+  //   );
+  //   let parsedData = await data.json();
+  //   setArticle(parsedData.articles);
+  // };
+
+  // const handlePrevClick = async () => {
+  //   commonUse();
+  //   setPage(page - 1);
+  // };
+
+  // const handleNextClick = async () => {
+  //   if (!(page + 1 > Math.ceil(totalResults / props.pageSizes))) {
+  //     commonUse();
+  //     setPage(page + 1);
+  //   }
+  // };
+
+
+  const fetchMoreData = async () => {
     let data = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${
-        props.category
-      }&apiKey=160b435514254bb28c355f4814de1b80&page=${page}&pageSize=${props.pageSizes}`
+      `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=160b435514254bb28c355f4814de1b80&page=${page}&pageSize=${props.pageSizes}`
     );
-    setLoading(true);
     let parsedData = await data.json();
-    setArticle(parsedData.articles);
-    setLoading(false);
-  }
-
-  const handlePrevClick = async () => {
-    commonUse();
-    setPage(page - 1);
-  };
-
-  const handleNextClick = async () => {
-    if (!(page + 1 > Math.ceil(totalResults / props.pageSizes))) {
-      commonUse();
-      setPage(page + 1);
-    }
+    setArticle(article.concat(parsedData.articles));
+    setPage(page + 1);
   };
 
   return (
-    <div className="container my-3">
+    <>
+    {/* // <div className="container my-3"> */}
       <h1 className="text-center" style={{ margin: '35px 0px' }}>
         New App - Top {capitalizeFirstLatter(props.category)} HeadLines
       </h1>
-      {loading}
-
-      <div className="row">
-        {!loading &&
-          article.map((element) => {
+      <InfiniteScroll
+        dataLength={article.length}
+        next={fetchMoreData}
+        hasMore={article.length !== totalResults}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="container">
+        <div className="row">
+          {article.map((element) => {
             return (
               <div className="col-md-4 mb-3" key={element.url}>
                 <NewsItem
@@ -73,8 +84,10 @@ const News = (props) => {
               </div>
             );
           })}
-      </div>
-      <div className="container d-flex justify-content-between">
+          </div>
+          </div>
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between">
         <button disabled={page <= 1} type="button" className="btn btn-primary" onClick={handlePrevClick}>
           Previous
         </button>
@@ -86,8 +99,9 @@ const News = (props) => {
         >
           Next
         </button>
-      </div>
-    </div>
+      </div> */}
+        {/* </div> */}
+        </>
   );
 };
 
